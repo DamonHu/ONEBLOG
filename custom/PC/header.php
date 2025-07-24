d2ea77bdb903f77bae8d2cc6087c5d33ccd08bbf
Damon <dong765@qq.com>
2025-07-24 11:06:22 +0800

    优化导航栏样式

--- custom/PC/header.php
+++ custom/PC/header.php
@@ -30,7 +30,7 @@ foreach ($domains as $domain): ?>
 <link href="<?php $this->options->themeUrl('/assets/sdk/animate.compat.css'); ?>" rel="stylesheet"><!--动画效果-->
 <link href="//at.alicdn.com/t/c/font_3940454_u9s3lgsdiq.css" rel="stylesheet"/><!---图标库 iconfont.cn -->
 <link rel="stylesheet" href="<?php $this->options->themeUrl('/assets/sdk/fancybox3/jquery.fancybox.min.css'); ?>" /><!--灯箱效果-->
-<link href="<?php $this->options->themeUrl('/assets/css/PC.css?v=3.5.3.5'); ?>" rel="stylesheet"/><!--主题核心样式-->
+<link href="<?php $this->options->themeUrl('/assets/css/PC.css?v=3.5.3.6'); ?>" rel="stylesheet"/><!--主题核心样式-->
 <style>
 :root {
     --theme-color: <?php $color = $this->options->themeColor;echo $color ? $color : '#ff5050';?>;
